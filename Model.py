6bc1746b318fa07cc6db7766428ed892c67ffb45
윤남식 <61834114+Namsik-Yoon@users.noreply.github.com>
2020-11-27 00:09:29 +0900

    Add files via upload

--- Model.py
+++ Model.py
@@ -156,7 +156,7 @@ class Encoder(nn.Module):
         super().__init__()
         self.config = config
         self.enc_emb = nn.Embedding(self.config.n_enc_vocab, self.config.d_hidn)
-        sinusoid_table = torch.FloatTensor(get_sinusoid_encoding_table(self.config.n_enc_seq + 1, self.config.d_hidn))
+        sinusoid_table = torch.FloatTensor(get_sinusoid_encoding_table(self.config.n_enc_seq+1, self.config.d_hidn))
         self.pos_emb = nn.Embedding.from_pretrained(sinusoid_table, freeze=True)
 
         self.layers = nn.ModuleList([EncoderLayer(self.config) for _ in range(self.config.n_layer)])
@@ -164,7 +164,7 @@ class Encoder(nn.Module):
     def forward(self, inputs):
         positions = torch.arange(inputs.size(1), device=inputs.device, dtype=inputs.dtype).expand(inputs.size(0),
                                                                                                   inputs.size(
-                                                                                                      1)).contiguous() + 1
+                                                                                                      1)).contiguous()+1
         pos_mask = inputs.eq(0)
         positions.masked_fill_(pos_mask, 0)  ## 체크
         # (bs, n_enc_seq, d_hidn)
@@ -348,3 +348,43 @@ class Classification(nn.Module):
         logits = self.projection(dec_outputs)
         # (bs, n_output), [(bs, n_head, n_enc_seq, n_enc_seq)], [(bs, n_head, n_dec_seq, n_dec_seq)], [(bs, n_head, n_dec_seq, n_enc_seq)]
         return logits, enc_self_attn_probs, dec_self_attn_probs, dec_enc_attn_probs
+    
+    
+""" Conv1dRnn """
+
+class Conv1dRNN(nn.Module):
+    def __init__(self, hidden_dim, filter_size, dropout_rate, vocab_size, embedding_dim,padding, pre_trained_embedding=None):
+        super().__init__()
+
+        self.hidden_dim = hidden_dim
+        self.filter_size = filter_size
+        self.dropout_rate = dropout_rate
+        self.embedding_dim = embedding_dim
+        self.padding = padding
+        
+        if pre_trained_embedding is None:
+            self.vocab_size = vocab_size
+            self.embedding = nn.Embedding(self.vocab_size, self.embedding_dim, padding_idx=0)
+        else:
+            self.embedding = nn.Embedding.from_pretrained(pre_trained_embedding, freeze=False, padding_idx=0)
+        self.gelu = nn.GELU()
+        self.dropout = nn.Dropout(self.dropout_rate)
+        self.conv1d = nn.Conv1d(self.embedding_dim, self.hidden_dim, self.filter_size, padding=self.padding)
+        self.bi_rnn = nn.LSTM(self.hidden_dim, int(self.hidden_dim / 2), batch_first=True, bidirectional=True)
+        self.uni_rnn = nn.LSTM(self.hidden_dim, self.hidden_dim, batch_first=True)
+        self.max_pool = nn.AdaptiveAvgPool2d((1, self.hidden_dim))
+        self.linear = nn.Linear(self.hidden_dim, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.embedding(x).transpose(0, 1).transpose(1, 2)
+        x = self.conv1d(x).transpose(1, 2).transpose(0, 1)
+        x = self.gelu(x)
+        x = self.dropout(x)
+        x_res = x
+        x, _ = self.bi_rnn(x)
+        x, _ = self.uni_rnn(x + x_res)
+        x = self.dropout(x)
+        x, _ = torch.max(x, 0)
+        x = self.linear(x)
+        return x
