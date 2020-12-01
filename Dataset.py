c41e760c907eda27633af2df96307df88b06525a
윤남식 <61834114+Namsik-Yoon@users.noreply.github.com>
2020-12-01 15:56:27 +0900

    Add files via upload

--- Dataset.py
+++ Dataset.py
@@ -3,17 +3,35 @@ import zipfile
 import pandas as pd
 import sentencepiece as spm
 
+from nltk.corpus import stopwords
+
+stopwords_en = set(stopwords.words('english'))
+
 import torch
-from transformers import AutoTokenizer
 from torch.utils.data import Dataset
 
 with zipfile.ZipFile('data/open.zip', 'r') as zip_ref:
     zip_ref.extractall('data/')
 
+
+def alpha_num(text):
+    return re.sub(r'[^A-Za-z0-9 ]', '', text)
+
+
+def remove_stopwords(text):
+    final_text = []
+    for i in text.split():
+        if i.strip().lower() not in stopwords_en:
+            final_text.append(i.strip())
+    return " ".join(final_text)
+
+
 class TextDataset(Dataset):
-    def __init__(self, vocab_size, separation=True):
+    def __init__(self, vocab_size, separation=True, train=True):
+        self.train = train
         self.input_file = 'spm_input.txt'
         self.df = pd.read_csv('data/train.csv')
+        self.df.text = self.df.text.str.lower().apply(alpha_num).apply(remove_stopwords)
         if separation:
             self.corpus = []
             self.authors = []
@@ -21,8 +39,7 @@ class TextDataset(Dataset):
                 texts = self.df.text[idx].split('. ')
                 author = self.df.author[idx]
                 for text in texts:
-                    text = re.sub(r'[^A-Za-z ]', '', text)
-                    self.corpus.append(text.lower())
+                    self.corpus.append(text)
                     self.authors.append(author)
         else:
             self.corpus = self.df.text.values.tolist()
@@ -45,16 +62,36 @@ class TextDataset(Dataset):
 
         self.sentences = []
         self.labels = []
-        for item, label in zip(self.corpus,self.authors):
+        for item, label in zip(self.corpus, self.authors):
             self.sentences.append(self.vocab.encode_as_ids(item))
             self.labels.append(label)
+        if not train:
+            self.df = pd.read_csv('data/test_x.csv', index_col='index')
+            self.df.text = self.df.text.str.lower().apply(alpha_num).apply(remove_stopwords)
+            if separation:
+                self.corpus = []
+                for idx in self.df.index:
+                    texts = self.df.text[idx].split('. ')
+                    for text in texts:
+                        self.corpus.append(text)
+            else:
+                self.corpus = self.df.text.values.tolist()
+            self.sentences = []
+            for item in self.corpus:
+                self.sentences.append(self.vocab.encode_as_ids(item))
 
     def __len__(self):
-        assert len(self.labels) == len(self.sentences)
-        return len(self.sentences)
+        if self.train:
+            assert len(self.labels) == len(self.sentences)
+            return len(self.sentences)
+        else:
+            return len(self.sentences)
 
     def __getitem__(self, idx):
         enc_inputs = torch.tensor(self.sentences[idx]).long()
         dec_inputs = torch.tensor([self.vocab.piece_to_id("[BOS]")])
-        label = torch.tensor(self.labels[idx])
-        return enc_inputs, dec_inputs, label
+        if self.train:
+            label = torch.tensor(self.labels[idx])
+            return enc_inputs, dec_inputs, label
+        else:
+            return enc_inputs, dec_inputs
